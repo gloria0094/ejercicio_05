4003ad02c4fb2e4508d655929604d8f827e420c9
gloria0094 <144404571+gloria0094@users.noreply.github.com>
2025-10-13 22:45:34 -0600

    Delete practica_05 directory

--- practica_05/src/main/java/com/upiiz/practica_05/repositories/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upiiz.practica_05.repositories;
-
-import com.upiiz.practica_05.entities.Pedido;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PedidoRepository extends CrudRepository<Pedido, Integer> {
-}
--- practica_05/src/main/java/com/upiiz/practica_05/repositories/PlatilloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upiiz.practica_05.repositories;
-
-import com.upiiz.practica_05.entities.Platillo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlatilloRepository extends CrudRepository<Platillo, Long> {
-}
